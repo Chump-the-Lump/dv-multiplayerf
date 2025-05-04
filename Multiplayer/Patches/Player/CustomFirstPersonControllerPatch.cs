535dd420e4564e38eb8de269018da0de79a80991
AMacro <andrew@andrewcraigmackenzie.com>
2025-05-05 00:26:49 +1000

    Add crouch and sit poses

--- Multiplayer/Patches/Player/CustomFirstPersonControllerPatch.cs
+++ Multiplayer/Patches/Player/CustomFirstPersonControllerPatch.cs
@@ -16,9 +16,16 @@ public static class CustomFirstPersonControllerPatch
     private static Vector3 lastPosition;
     private static float lastRotationY;
     private static bool sentFinalPosition;
+    private static bool wasCrouching;
+    private static bool wasSitting;
+    private static bool wasSwimming;
+    
 
     private static bool isJumping;
     private static bool isOnCar;
+    private static bool isCrouching;
+    private static bool isSitting;
+    private static float sitHeight;
     private static TrainCar car;
 
     [HarmonyPatch(nameof(CustomFirstPersonController.Awake))]
@@ -60,12 +67,47 @@ public static class CustomFirstPersonControllerPatch
 
         bool positionOrRotationChanged = Vector3.Distance(lastPosition, position) > 0 || Math.Abs(lastRotationY - rotationY) > ROTATION_THRESHOLD;
 
+        //Multiplayer.LogDebug(() =>$"Player OnTick() Position: {position}, Last position:{lastPosition}, Rotation: {rotationY}, Last rotation: {lastRotationY}, Changed: {positionOrRotationChanged}");
+
         if (!positionOrRotationChanged && sentFinalPosition)
             return;
 
         lastPosition = position;
         lastRotationY = rotationY;
         sentFinalPosition = !positionOrRotationChanged;
+
+        if (fps.IsCrouching)
+        {
+            wasCrouching = true;
+            Multiplayer.LogDebug(() =>$"Crouch");
+        }
+        else if(wasCrouching)
+        {
+            wasCrouching = false;
+            Multiplayer.LogDebug(() => $"Stand from crouch");
+        }
+
+        if (fps.provider.IsSitting)
+        {
+            wasSitting = true;
+            Multiplayer.LogDebug(() => $"Sit");
+        }
+        else if (wasSitting)
+        {
+            wasSitting = false;
+            Multiplayer.LogDebug(() => $"Stand from sit");
+        }
+
+        if (fps.underwater)
+        {
+            wasSwimming = true;
+            Multiplayer.LogDebug(() => $"Swim");
+        }
+        else if (wasSwimming)
+        {
+            wasSwimming = false;
+            Multiplayer.LogDebug(() => $"Stand from swim");
+        }
 
         ushort carNetID = isOnCar ? car.GetNetId() : (ushort)0;
 
--- MultiplayerAssets/Assets/Scripts/Multiplayer/Components/Player/AnimationHandler.cs
+++ MultiplayerAssets/Assets/Scripts/Multiplayer/Components/Player/AnimationHandler.cs
@@ -11,6 +11,8 @@ namespace Multiplayer.Editor.Components.Player
         private static readonly int hash_Jump = Animator.StringToHash("Jump");
         private static readonly int hash_Vertical = Animator.StringToHash("Vertical");
         private static readonly int hash_Horizontal = Animator.StringToHash("Horizontal");
+        private static readonly int hash_Crouch = Animator.StringToHash("Crouch");
+        private static readonly int hash_Sit = Animator.StringToHash("Sit");
 
         [UsedImplicitly]
         public void SetIsJumping(bool isJumping)
@@ -19,10 +21,49 @@ namespace Multiplayer.Editor.Components.Player
         }
 
         [UsedImplicitly]
+        public void SetIsCrouching(bool isCrouching)
+        {
+            animator.SetBool(hash_Crouch, isCrouching);
+        }
+
+        [UsedImplicitly]
+        public void SetIsSitting(bool isSitting)
+        {
+            animator.SetBool(hash_Sit, isSitting);
+        }
+
+        [UsedImplicitly]
         public void SetMoveDir(Vector2 moveDir)
         {
             animator.SetFloat(hash_Horizontal, moveDir.x);
             animator.SetFloat(hash_Vertical, moveDir.y);
         }
+
+#if UNITY_EDITOR
+        bool isJumping;
+        bool isCrouching;
+        bool isSitting;
+        [UsedImplicitly]
+        void Update()
+        {
+            if (Input.GetButton("Jump"))
+            {
+                isJumping = !isJumping;
+                SetIsJumping(isJumping);
+            }
+            else if (Input.GetButton("Crouch"))
+            {
+                isCrouching = !isCrouching;
+                Debug.Log($"Crouch {isCrouching}");
+                SetIsCrouching(isCrouching);
+            }
+            else if (Input.GetButton("Sit"))
+            {
+                isSitting = !isSitting;
+                Debug.Log($"Sit {isSitting}");
+                SetIsCrouching(isCrouching);
+            }
+        }
+#endif
     }
 }
